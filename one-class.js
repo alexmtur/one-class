57246c5c1f821adc8aea4a4e9cff08e8bcf33411
Alex Martin <alexmartinturrillas@gmail.com>
2018-10-01 07:46:01 +0200

    script update

--- one-class.js
+++ one-class.js
@@ -416,3 +416,222 @@ export class OneLink extends OneClass {
     }
 }
 customElements.define('one-link', OneLink);
+
+export class OneBlock extends OneClass {
+    static get properties() {return {
+        //Layout Properties
+        align: {type: String, value: 'top-left'},
+        direction: {type: String, value: 'row'},
+        weight: {type: Number, value: 0},
+        width: {type: String, value: 'auto'},
+        height: {type: String,value: 'auto'},
+    }}
+    constructor() {
+        super();
+        this.align = 'top-left';
+        this.direction = 'row';
+        this.weight = 0;
+    }
+    setLayout() {
+        //Alignment:
+        let index = this.align.indexOf('-');
+        let vAlign = this.align.slice(0, index);
+        let hAlign = this.align.slice(index + 1, this.align.length);
+        let isRow = true;
+
+        if(this.direction === 'column' || this.direction === 'column-reverse') {
+            let aux = vAlign;
+            vAlign = hAlign;
+            hAlign = aux;
+            isRow = false;
+        }
+
+        if(vAlign === 'center') {
+            this.vAlign = vAlign;
+            //this.updateStyles({'--one-vertical-alignment': vAlign});
+        }
+        else if(vAlign === 'bottom') {
+            this.vAlign = 'flex-end';
+            //this.updateStyles({'--one-vertical-alignment': 'flex-end'});
+        }
+        else if(vAlign === 'space') {
+            if(!isRow) {
+                this.vAlign = 'space-around';
+                //this.updateStyles({'--one-vertical-alignment': 'space-around'});
+            }
+        }
+        else if(vAlign === 'stretch') {
+            if(isRow) {
+                this.vAlign = vAlign;
+                //this.updateStyles({'--one-vertical-alignment': 'stretch'});
+            }
+            else {
+                this.vAlign = 'space-between';
+                //this.updateStyles({'--one-vertical-alignment': 'space-between'});
+            }
+
+        }
+        else {
+            this.vAlign = 'flex-start'
+            //this.updateStyles({'--one-vertical-alignment': 'flex-start'});
+        }
+
+        if(hAlign === 'center') {
+            this.hAlign = hAlign;
+            //this.updateStyles({'--one-horizontal-alignment': 'center'});
+        }
+        else if(hAlign === 'right') {
+            this.hAlign = 'flex-end';
+            //this.updateStyles({'--one-horizontal-alignment': 'flex-end'});
+        }
+        else if(hAlign === 'space') {
+
+            if(isRow) {
+                this.hAlign = 'space-around';
+                //this.updateStyles({'--one-horizontal-alignment': 'space-around'});
+            }
+        }
+        else if(hAlign === 'stretch') {
+            if(isRow) {
+                this.hAlign = 'space-between';
+                //this.updateStyles({'--one-horizontal-alignment': 'space-between'});
+            }
+            else {
+                this.hAlign = hAlign;
+                //this.updateStyles({'--one-horizontal-alignment': 'stretch'});
+            }
+        }
+        else {
+            this.hAlign = 'flex-start';
+            //this.updateStyles({'--one-horizontal-alignment': 'flex-start'});
+        }
+
+
+        //Weight, width and height:
+        // this.updateStyles({'--one-weight': this.weight + ''});
+        // this.updateStyles({'--one-width': this.width});
+        // this.updateStyles({'--one-height': this.height});
+    }
+    _firstRendered() {
+        super._firstRendered();
+        this.setLayout();       
+    }
+    _render() {return html`
+        <style>
+            :host {
+                display: flex;
+                justify-content: ${this.hAlign};
+                align-items: ${this.vAlign};
+                flex-direction: ${this.direction};
+                flex: ${this.weight};
+            }
+        </style>dsfs
+        <slot></slot>`
+    }
+}
+customElements.define('one-block', OneBlock);
+
+export const oneStyle = html`
+<!-- <svg class="one-icon" style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
+  <defs>
+    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
+      <stop offset="0%" style="stop-color:#43CBFF;stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#9708CC;stop-opacity:1" />
+    </linearGradient>
+  </defs>
+</svg> -->
+<style>
+    /** SET UP THE FOLLOWING VARIABLES IN THE INEX BODY:
+        --one-primary-color (one-color and one-background)
+        --one-secondary-color //maybe not needed, set up locally?
+        --one-border-radius
+        --one-border
+        --one-size
+        --one-font
+        --
+
+        //create a file to import in the index with the body setup
+
+
+    **/
+    * {
+        font: 100% "Lato", sans-serif;
+        font-weight: 300 !important;
+        color: #333;
+     }
+    one-icon {
+        width: 55px; height: auto;
+        color: blue;
+        --iron-icon-height: 52px;
+        --iron-icon-width: 52px;
+        /*--iron-icon-fill-color: (#grad1);*/
+        fill: url(#one-gradient);
+        /*fill: url(#grad1);
+        stroke: #f1f1f1;
+        stroke-width: 1px;
+        stroke-linejoin: round;*/
+    }
+    /*one-navbar {*/
+        /*position: fixed;*/
+        /*top: 0px;*/
+        /*left: 0px;*/
+        /*width: 100%;*/
+        /*z-index: 500;*/
+        /*!*--one-background: green;*!*/
+        /*!*background-color: pink;*!*/
+    /*}*/
+    one-navbar-item {
+        width: 40px;
+        --one-icon-size: 30px;
+        /*background: blue;*/
+    }
+
+    button {
+        font-family: inherit;
+        font-weight: inherit;
+        font-size: 100%;
+        background: var(--one-background, #333);
+        border-radius: 0px;
+        border: 0px;
+        padding: 10px 15px 10px 15px;
+        color: white;
+        cursor: pointer;
+
+    }
+    button:hover {
+        opacity: 0.8;
+    }
+    button:focus {
+        outline: none;
+    }
+    a {
+        text-decoration: none !important;
+        color: white;
+    }
+    input {
+        background: grey !important;
+        border: solid 1px var(--one-color, #333) !important;
+        color: white;
+        font: 100% "Lato", sans-serif;
+        display: inline-block;
+        /*border-radius: 20px;*/
+        text-align: center;
+        line-height: 35px;
+        min-width: 100px;
+    }
+    input:focus {
+        outline: none;
+    }
+    input[type=text] {
+        width: 100%;
+        max-width: 350px;
+    }
+    input {
+       
+    }
+    input:required:invalid, input:focus:invalid {
+      /* this should be styled by the user in the stylesheet */
+      /*check the project from IMPACT*/
+      
+    }
+</style>`;
